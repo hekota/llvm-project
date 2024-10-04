aae09e5d7622181970755d0260d7f3e8b68aba11
Helena Kotas <hekotas@microsoft.com>
2024-10-04 13:52:47 -0700

    clang-format
    (cherry picked from commit a6c06943ce5df79e6765e12874c96c907b20d030)

--- clang/lib/Sema/SemaHLSL.cpp
+++ clang/lib/Sema/SemaHLSL.cpp
@@ -2214,7 +2214,7 @@ QualType SemaHLSL::getInoutParameterType(QualType Ty) {
 void SemaHLSL::ProcessResourceBindingOnDecl(VarDecl *D) {
   if (!D->hasGlobalStorage())
     return;
-  
+
   for (Attr *A : D->attrs()) {
     HLSLResourceBindingAttr *RBA = dyn_cast<HLSLResourceBindingAttr>(A);
     if (!RBA)
